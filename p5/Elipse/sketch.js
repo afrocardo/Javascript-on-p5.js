104df22c2a226eccd73b1258e1b3c29eb8215654
Ricardo José Meza Díaz <rmezadia@alumnos.unex.es>
2017-11-05 13:39:11 +0100

    Update sketch.js

--- p5/Elipse/sketch.js
+++ p5/Elipse/sketch.js
@@ -1,7 +1,10 @@
 function setup() {
-  // put setup code here
+  createCarnvas(400,400);
 }
 
 function draw() {
+  //Fondo de color rojo
+  background(255,0,0);
+  //Crea una elipse
   ellipse(50,50,80,80);
 }
